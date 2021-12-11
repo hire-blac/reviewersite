09aacb25102df18c6768629c17329c746c2f64a7
danihire@gmail.com <danihire@gmail.com>
2021-12-11 11:19:12 +0100

    removed static dir route

--- ReviewerNG/settings.py
+++ ReviewerNG/settings.py
@@ -150,6 +150,6 @@ STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'static/images')
 
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static')
-]
+# STATICFILES_DIRS = [
+#     os.path.join(BASE_DIR, 'static')
+# ]
