6b1ff69d74862e2387a2f2a9a48674356ef583cd
danihire@gmail.com <danihire@gmail.com>
2022-01-25 03:43:41 +0100

    static

--- ReviewerNG/settings.py
+++ ReviewerNG/settings.py
@@ -161,9 +161,9 @@ STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'reviewer/static/images')
 
-# STATICFILES_DIRS = [
-#     os.path.join(BASE_DIR, 'static')
-# ]
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static')
+]
 
 # DJANGO ALL-AUTH
 
