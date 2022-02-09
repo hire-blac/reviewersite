1ec38f6adfd0e7578d0963ecd38589760c11e2fa
danihire@gmail.com <danihire@gmail.com>
2022-02-09 21:44:28 +0100

    styling and login

--- reviewer/templatetags/my_filters.py
+++ reviewer/templatetags/my_filters.py
@@ -9,3 +9,17 @@ def stars(num):
 @register.filter(name='length')
 def length(follow):
     return len(follow)
+
+@register.filter(name='vote_count')
+def vote_count(votes):
+    if votes == 0:
+        return ''
+    elif votes >= 1000000:
+        count = votes / 1000000
+        return f'{count:.2f}M'
+    elif votes >= 1000:
+        count = votes / 1000
+        return f'{count:.2f}K'
+    else:
+        return f'{votes}'
+        
