7f84c50c3f34f338d3b7d2e3d5644917bfa9b918
danihire@gmail.com <danihire@gmail.com>
2022-03-19 22:03:41 +0100

    review slug

--- review/admin.py
+++ review/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
 from . models import Comment, Review, Vote
 
-# Register your models here.
+# Register your models here
+
 admin.site.register(Review)
 admin.site.register(Vote)
 admin.site.register(Comment)
