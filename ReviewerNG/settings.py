257ad44b28e81c1be964c6a9733de1fcae72806f
danihire@gmail.com <danihire@gmail.com>
2022-05-10 14:43:18 +0100

    keyss

--- ReviewerNG/settings.py
+++ ReviewerNG/settings.py
@@ -23,7 +23,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-# SECRET_KEY = 'django-insecure-6cu*c#y-_18f%-hso&5au&$jdxi@75!@lcebl@(_tunn7kb9v#'
 import os
 SECRET_KEY = os.environ['SECRET_KEY']
 
